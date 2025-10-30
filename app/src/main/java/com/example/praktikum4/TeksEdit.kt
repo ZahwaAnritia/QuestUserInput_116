1517859daf1cf08983a0eab017043622fea23f3e
ZahwaAnritia <anritia15@gmail.com>
2025-10-30 16:06:18 +0700

    tambahkan import untuk state management (remember, mutableStateOf, getValue, setValue) pada FormDataDiri

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -13,6 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+
 import java.nio.file.WatchEvent
 
 
@@ -99,7 +105,5 @@ fun FormDataDiri(modifier: Modifier)
         }
 
     }
-
-
 
 }
