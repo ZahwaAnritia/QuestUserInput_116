9b72221ee793a1a0e10ca46ecf72b32f54b4876a
ZahwaAnritia <anritia15@gmail.com>
2025-10-30 15:21:37 +0700

    tambah daftar pilihan gender

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -16,5 +16,7 @@ fun FormDataDiri(modifier: Modifier)
     var alamat by remember { mutableStateOf("") }
     var jenis by remember { mutableStateOf("") }
 
+    val gender: List<String> = listOf("Laki-laki", "Perempuan")
+
 
 }
