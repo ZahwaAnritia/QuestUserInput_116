68ab474d9a9ea8829d2e6d3535f9248d73f90dce
ZahwaAnritia <anritia15@gmail.com>
2025-11-01 08:26:43 +0700

    menambahkan variabel state untuk form

---
+++ app/src/main/java/com/example/praktikum4/TeksEdit2.kt
@@ -0,0 +1,19 @@
+package com.example.praktikum4
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun FormDataDiri2(modifier: Modifier = Modifier){
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("")}
+
+
+
