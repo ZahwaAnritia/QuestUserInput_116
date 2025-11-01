039c25da700e9c2ca094bf634820637d21fd4227
ZahwaAnritia <anritia15@gmail.com>
2025-11-01 08:32:21 +0700

    menambahkan warna tema

--- app/src/main/java/com/example/praktikum4/TeksEdit2.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit2.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 
 @Composable
@@ -24,6 +25,10 @@ fun FormDataDiri2(modifier: Modifier = Modifier) {
 
     val gender: List<String> = listOf("Laki-laki", "Perempuan")
     val statusPerkawinan: List<String> = listOf("Janda", "Lajang", "Duda")
+
+    val purpleBackground = Color(0xFFE6D5F5)
+    val purpleButton = Color(0xFF7C3AED)
+    val purpleHeader = Color(0xFFB794F6)
 
 }
 
