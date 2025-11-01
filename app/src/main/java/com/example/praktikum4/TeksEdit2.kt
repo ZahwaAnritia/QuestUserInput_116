9eebcf093e3d82dafc651f203e31d8463dd4a23c
ZahwaAnritia <anritia15@gmail.com>
2025-11-01 08:28:38 +0700

    menambahkan variabel state untuk menyimpan data form

--- app/src/main/java/com/example/praktikum4/TeksEdit2.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit2.kt
@@ -9,11 +9,18 @@ import androidx.compose.ui.Modifier
 
 
 @Composable
-fun FormDataDiri2(modifier: Modifier = Modifier){
+fun FormDataDiri2(modifier: Modifier = Modifier) {
     var textNama by remember { mutableStateOf("") }
     var textAlamat by remember { mutableStateOf("") }
     var textJK by remember { mutableStateOf("") }
-    var textStatus by remember { mutableStateOf("")}
+    var textStatus by remember { mutableStateOf("") }
+
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+
+}
 
 
 
